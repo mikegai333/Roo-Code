0dc46edba9761377eef451467d370a3eb589ea52
tangs <tangshunrong@hotmail.com>
2025-08-19 22:33:47 +0800

    feat: 增加留存率计算主流程

--- src/extension/providers/completion.ts
+++ src/extension/providers/completion.ts
@@ -428,7 +428,13 @@ export class CompletionProvider implements InlineCompletionItemProvider {
 		try {
 			const logContext = new InlineEditRequestLogContext(doc.id.uri, document.version, context)
 
-			const diagnosticsPromise = this.diagnosticsProvider.runUntilNextEdit(doc.id, context, logContext, 50, token)
+			const diagnosticsPromise = this.diagnosticsProvider.runUntilNextEdit(
+				doc.id,
+				context,
+				logContext,
+				500,
+				token,
+			)
 
 			const timeoutPromise = new Promise<undefined>((resolve) => setTimeout(() => resolve(undefined), 750))
 
@@ -840,7 +846,7 @@ export class CompletionProvider implements InlineCompletionItemProvider {
 		// return this._extensionContext.globalState.get<TwinnyProvider>(
 		//   ACTIVE_FIM_PROVIDER_STORAGE_KEY
 		// );
-		const env: string = "test1"
+		const env: string = "test"
 		if (env === "test") {
 			return {
 				apiHostname: "192.168.163.1",
